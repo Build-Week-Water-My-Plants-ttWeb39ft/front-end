a3bb81a5c6d19d78e3b7189a133ab8267f899bbb
Aszalea Calderon <aszalea_calderon@yahoo.com>
2020-11-16 14:04:40 -0800

    valid

--- plant-app/src/App.js
+++ plant-app/src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import "./App.css";
+import Signup from "./components/Signup.js";
 
 function App() {
   return (
     <div className="App">
       <h1>LETS WATER SOME MF'N PLANTS</h1>
+      <Signup />
     </div>
   );
 }

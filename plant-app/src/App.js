1a75764775b24716cb7bae91cd5ef01c791eb2a3
reesharper <reestimharper@gmail.com>
2020-11-17 17:59:50 -0500

    added protected route to protected page and added a dope ass favicon

--- plant-app/src/App.js
+++ plant-app/src/App.js
@@ -4,6 +4,8 @@ import AddPlant from './components/AddPlant';
 import Login from "./components/Login.js";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import Home from "./components/Home.js";
+import ProtectedRoute from './components/ProtectedRoute';
+import { ProtectedPage } from "./components/ProtectedPage";
 
 
 function App() {
@@ -15,6 +17,7 @@ function App() {
           <Route path="/" exact component={Home} />
           <Route exact path="/Signup" component={Signup} />
           <Route exact path="/Login" component={Login}></Route>
+          <ProtectedRoute exact path = "/My-plants" component={ProtectedPage} />
         </Switch>
       </div>
     </Router>

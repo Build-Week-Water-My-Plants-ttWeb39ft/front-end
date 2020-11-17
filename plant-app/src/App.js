b16eafa9848cb02f305209096e74df340a80ca30
Aszalea Calderon <aszalea_calderon@yahoo.com>
2020-11-17 14:44:29 -0800

    added styles and things and stuff

--- plant-app/src/App.js
+++ plant-app/src/App.js
@@ -1,14 +1,20 @@
 import "./App.css";
 import Signup from "./components/Signup.js";
 import Login from "./components/Login.js";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import Home from "./components/Home.js";
 
 function App() {
   return (
-    <div className="App">
-      <h1>LETS WATER SOME MF'N PLANTS</h1>
-      <Login />
-      <Signup />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route exact path="/Signup" component={Signup} />
+          <Route exact path="/Login" component={Login}></Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 

0e56db858be3dbf781f490bf344f24eadaa3c2b2
reesharper <reestimharper@gmail.com>
2020-11-17 23:14:57 -0500

    added some auth as well as adjusted protected page format

--- plant-app/src/components/ProtectedPage.js
+++ plant-app/src/components/ProtectedPage.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { axiosWithAuth } from './axiosWithAuth';
+import { Link } from "react-router-dom";
 
 export const ProtectedPage = () => {
 
@@ -15,12 +16,33 @@ export const ProtectedPage = () => {
     })
   },[]);
 
+  const deletePlant = plant => {
+    axiosWithAuth()
+    .delete(`/plants/${plant.id}`)
+    .then(req => {
+      setPlants(req.data);
+      window.location.reload();
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }
+
   return(
     <div className="plants-container">
       <h1>Your Plants</h1>
 
+      <div className="newPlant">
+      <Link to="/Add-plant">
+          <button type="button">
+            Add Plant
+          </button>
+      </Link>
+      </div>
+
       <div className="plants">
         {plants.map(plant => (
+
           <div className="plant">
           <h2>{plant.nickname}</h2>
           <img alt={plant.description}>{plant.image}</img>
@@ -28,8 +50,14 @@ export const ProtectedPage = () => {
           <p>{plant.description}</p>
           <p>{plant.datePlanted}</p>
           <p>{plant.frequency}</p>
+          <p>{plant.days}</p>
           <p>{plant.careInstructions}</p>
+          <button onClick={e => {
+            e.stopPropagation();
+            deletePlant(plant);
+          }}>Delete</button>
           </div>
+
         ))}
       </div>
     </div>

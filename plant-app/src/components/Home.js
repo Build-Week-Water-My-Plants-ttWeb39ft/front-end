42b248a1441874e1971122fc1fd5a4299d7c2c60
Jay Ponce de Leon <20601752+jaypdl@users.noreply.github.com>
2020-11-18 11:22:35 -0700

    Revert "Styled signup to be closer to login format"

--- plant-app/src/components/Home.js
+++ plant-app/src/components/Home.js
@@ -6,26 +6,18 @@ function Home() {
   return (
     <>
       <div className="main">
-        <div className="logo"></div>
-        <div className="furtherDown">
-          <h1>The Plant App</h1>
-          <Link to="/Login">
-            <button type="button">Login</button>
-          </Link>
-          <br></br>
-          <Link to="/Signup">
-            <button type="button">Sign up!</button>
-          </Link>
-        </div>
+        <h1>THE Plant App</h1>
+        <Link to="/Login">
+          <button type="button">
+            Login
+          </button>
+        </Link>
+        <Link to="/Signup">
+          <button type="button">
+            Sign up!
+          </button>
+        </Link>
       </div>
-      {/* <div>
-        <h2>Real reviews</h2>
-        <p>I would use this!</p>
-      </div> */}
-      {/* Footer */}
-      {/* <footer>
-        <p>Cool Plant App &#169; 2020</p>
-      </footer> */}
     </>
   );
 }

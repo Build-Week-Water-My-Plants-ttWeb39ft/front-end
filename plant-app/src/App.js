1f364c197a058d89b2388203370cbbf2bcc225f3
Jay Ponce de Leon <jayponcedeleon@gmail.com>
2020-11-19 03:01:10 -0700

    Added EditPlant, bug: resets to original days when hitting submit

--- plant-app/src/App.js
+++ plant-app/src/App.js
@@ -1,7 +1,6 @@
 import "./App.css";
 import Signup from "./components/Signup.js";
 import AddPlant from "./components/AddPlant";
-import AddPlantNew from "./components/AddPlantNew";
 import Login from "./components/Login.js";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import Home from "./components/Home.js";
@@ -18,7 +17,6 @@ function App() {
           <Route path="/" exact component={Home} />
           <Route exact path="/Signup" component={Signup} />
           <Route exact path="/Login" component={Login} />
-          <Route path="/Add-plant-new" component={AddPlantNew} />
           <Route path="/Add-plant" component={AddPlant} />
           <Route path="/Edit-plant" component={EditPlant}/>
           <ProtectedRoute exact path="/My-plants" component={ProtectedPage} />
